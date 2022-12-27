fb12eddefdb3e0d7a00c39846cf5f0cd56939ec0
Sofiya-Chiffa <94921852+Sofiya-Chiffa@users.noreply.github.com>
2022-12-27 22:08:30 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
@@ -19,11 +20,28 @@ void print_field(int a){
     cout <<endl;
 }
 
-void hum_step(int a){
+int hum_id(string a){
+    int ch = 0;
+    while (true){
+        for (int i=0;i<a.length();++i){
+            if (!isdigit(a[0])){
+                ch = 1;
+                break;
+            }
+        }
+        if (ch == 0){
+            return stoi(a);
+        }
+    }
+}
+
+void hum_step(string b){
+    int a = hum_id(b);
     int i = (a -1) / 3;
     int j = (a - 1) - ((a - 1) / 3 * 3);
     while (a > 9 || a < 1 || field[i][j] != '_'){
-        cin >> a;
+        cin >> b;
+        a = hum_id(b);
         i = (a -1) / 3;
         j = (a - 1) - ((a - 1) / 3 * 3);
     }
@@ -152,6 +170,10 @@ int check(int a){
 
         return 1;
     }
+    if (field[0][0] != '_' && field[0][1] != '_' && field[0][2] != '_' && field[1][0] != '_' &&
+    field[1][1] != '_' && field[1][2] != '_' && field[2][0] != '_' && field[2][1] != '_' && field[2][2] != '_'){
+        return -1;
+    }
     return 0;
 }
 
@@ -167,20 +189,26 @@ int main()
         pl = '0';
         pc = 'x';
     }
-    int b;
+    string b;
     while (true){
         print_field(0);
         cin >> b;
         hum_step(b);
         print_field(0);
-        if (check(0)){
+        if (check(0) == 1){
             cout << "x won";
+            break;}
+        if (check(0) == -1){
+            cout << "nobody win";
             break;
         }
         pc_step(0);
-        if (check(0)){
+        if (check(0) == 1){
             print_field(0);
             cout << "o won";
+            break;}
+        if (check(0) == -1){
+            cout << "nobody win";
             break;
         }
     }
